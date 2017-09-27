e4b478547cae8a05d8a78bb7baacba4c26757c75
SIVIV <stoyan.ivanoff87@gmail.com>
2017-09-28 01:08:51 +0300

    last commit for tonight

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,7 @@
-import com.sun.org.apache.xpath.internal.SourceTree;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-
-        System.out.println("Is this working?");
-
-        System.out.println("Helloooo");
-
-        System.out.println("I figured this shit out!");
-
-        System.out.println("NEW TEST");
-
-        System.out.println("OKOKOKOK");
+        System.out.println("Hello Darkness, my old friend!");
     }
 }
